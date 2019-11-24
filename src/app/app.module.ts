6a08c8ac9a3cd0608855c57ab110c31effaccc24
UjwalAnilMahule <ujwalmahule@cn.ibm.com>
2019-11-24 12:27:55 +0800

    create and update login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +26,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { BodyDirective } from './directives/body.directive';
 import { SignupPageComponent } from './components/signup-page/signup-page.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -21,8 +39,27 @@ import { SignupPageComponent } from './components/signup-page/signup-page.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    MatBadgeModule,
+    MatProgressBarModule,
     MatToolbarModule,
-    BrowserAnimationsModule
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
   ],
   entryComponents: [AppComponent, LoginPageComponent, SignupPageComponent],
   providers: [],
--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { BodyComponent } from './../../interfaces/body-component';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -8,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit, BodyComponent {
 
+  loginForm: FormGroup;
+
+  loading = false;
+  submitted = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    });
+    console.log("initialized");
+  }
+
+  hasError = (controlName: string, errorName: string) => {
+    return this.loginForm.controls[controlName].hasError(errorName);
+  }
+
+  login() {
+    this.loading = true;
+    console.log("OK");
+    
   }
 
 }
